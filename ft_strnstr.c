73cde4e29146f1b6dc8d9835ef9c11cd7b2bc8b6
Demitrus Booker <demitrusbooker@Demitrus-Bookers.local>
2020-03-15 17:02:44 -0700

    strnstrn

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,25 +14,23 @@
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
-    size_t i;
+	size_t i;
 	size_t j;
-
-    i = 0;
-    if (s2 == '\0')
-        return ((char *)s1);
-    while (s1[i] && i < len)
-    {
-		j = 0;
-        while (s1[i + j])
-        {
-            p++;
-            p2++;
-            i++;
-        }
-        if (*p2 == '\0')
-            return ((char *)s1);
-        s1++;
-        i++;
-    }
-    return (NULL);	
+	
+	i = 0;
+	if (s2[i] == '\0')
+		return ((char *)s1);
+	while (s1[i] && i < len)
+	{
+		if (s1[i] == s2[0])
+		{
+			j = 1;
+			while (s2[i] && s1[i + j] == s2[j] && (i + j) < len)
+				j++;
+			if (s2[j] == '\0')
+				return ((char *)&s1[i]);
+		}
+		i++;
+	}
+	return (NULL);
 }
